f050a544f10d0d527a0b26b28f60a85ed3aebf31
Santiago Anibal Yegros Zaracho <santiago.yegros@gmail.com>
2023-04-25 00:27:20 -0400

    Version 1.0.0.0.0.1

--- EDA.R
+++ EDA.R
@@ -11,21 +11,84 @@ if (!require('summarytools'))
   install.packages("summarytools")
 library(summarytools)
 
+if (!require('ISLR'))
+  install.packages("ISLR")
+library(ISLR)
 
-# Read the data from the RDS file
+if (!require('rio'))
+  install.packages("rio")
+library(rio)
+
+if (!require('explore'))
+  install.packages("explore")
+library(explore)
+
+
+# Leemos los datos de Marzo 2023
 transporte <- readRDS("data/202303.rds")
 rutas <- read.csv("data/rutas.csv")
 
-
+# Seleccionamos las columnas a utilizar
 trans <- select(transporte, -consecutivoevento, -dia_hora, -dia_semana, 
                 -fecha, -hora, -latitude, -longitude)
-
 rut <- select(rutas, -estado, -troncal)
-# Fusionar los datos utilizando la columna "idsam" como clave de unión
+
+# Fusionar los datos utilizando la columna "idrutaestacion" como clave de unión
 datos_fusionados <- inner_join(trans, rut, by = "idrutaestacion")
 
+# Split de Fecha y Hora
+datos_fusionados$Fecha <- as.Date(datos_fusionados$fechahoraevento)
+datos_fusionados$Hora <- format(datos_fusionados$fechahoraevento,"%H:%M:%S")
+
+
+datos_fusionados_short <- head(datos_fusionados, 1000)
+
+datos_fusionados_short <- datos_fusionados_short %>%
+                            mutate(Hora = hms(Hora))
+
+datos_fusionados_short$h <- as.numeric(format(datos_fusionados_short$fechahoraevento,'%H'))
+
+datos_fusionados_short %>%
+              mutate(Periodo = case_when(datos_fusionados_short$h >= 0 & datos_fusionados_short$h < 4 ~ 'Madrugada',
+                                         datos_fusionados_short$h >= 4 & datos_fusionados_short$h < 8 ~ 'Mañana',
+                                         datos_fusionados_short$h >= 8 & datos_fusionados_short$h < 12 ~ 'Medio Dia',
+                                         datos_fusionados_short$h >= 12 & datos_fusionados_short$h < 16 ~ 'Siesta',
+                                         datos_fusionados_short$h >= 16 & datos_fusionados_short$h < 20 ~ 'Tarde',
+                                         datos_fusionados_short$h >= 20 & datos_fusionados_short$h < 24 ~ 'Noche',
+                                        TRUE ~ 'Error'))
+
+
+datos_fusionados_short$Periodo <-
+              mutate(Periodo = case_when(datos_fusionados_short$h >= 20 & datos_fusionados_short$h < 24 ~ 'Noche'))
+
+
+#Groupby por tipo transporte: 1. Normal - 3. Diferencial
+datos_fusionados_t <- datos_fusionados %>%
+                      filter(tipoevento == 4) %>% 
+                      group_by(tipotransporte, EOT, ramal, Fecha) %>%
+                      summarize(cantidad_viajes = sum(montoevento)) %>%
+                      ungroup()
+
+datos_fusionados_short <- head(datos_fusionados, 1000)
+
+datos_fusionados_short %>%
+  eda_paged_report(target = "EOT", subtitle = "Empresas", 
+                   output_dir = "./output", output_file = "EDA.pdf", theme = "blue")
+
+#dfSummary(datos_fusionados_short)
+
+#explore(datos_fusionados_short)
+
+create_notebook_explore(
+  output_dir =  "./",
+  output_file = "output/notebook-exploracion-EDA")
+
+
+dlookr::describe(datos_fusionados)
 
 library(ISLR)
+
+dfSummary(datos_fusionados)
 st_options(lang = "es") #Translations
 summarytools::view(dfSummary(datos_fusionados), 
                    footnote = NA, 
@@ -50,5 +113,6 @@ datos_fusionados_t <- datos_fusionados %>%
                       group_by(tipotransporte, EOT, ramal) %>%
                       summarize(cantidad_viajes = sum(montoevento)) %>%
                       ungroup()
-  
+
+
 
